40c7131c1731e0680f7d261e4b8da110be611f81
Oliver Gomes <ogomesphoto@gmail.com>
2020-09-08 16:16:09 -0400

    Home Component Initialize

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/TV/THBY_S2_02111_GWBleedingHero_1500x600_PRE_Final_en-US_PVD5224._CB410800060_.jpg"
+          alt="banner"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
