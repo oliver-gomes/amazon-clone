6b24f8e93ab0647bb65577431a7934e286cb32b4
Oliver Gomes <ogomesphoto@gmail.com>
2020-09-16 14:13:23 -0400

    bounce animation add

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,99 @@
 import React from "react";
+import Noty from "noty";
+import Bounce from "bounce.js";
 
+import "../../../node_modules/noty/lib/noty.css";
+import "../../../node_modules/noty/lib/themes/mint.css";
 import "./Product.css";
-function Product({ title, image, rating, price }) {
+import { useStateValue } from "../../StateProvider";
+
+function Product({ id, title, image, rating, price }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+
+    new Noty({
+      text: `
+      <div style="display: flex; ">
+      <img src=${image} width="50px" style="margin-right: 15px"></img>
+      <p>${title}</p>
+      </div>
+      `,
+      timeout: 1000,
+      layout: "topRight",
+
+      animation: {
+        open: function (promise) {
+          var n = this;
+          new Bounce()
+            .translate({
+              from: { x: 450, y: 0 },
+              to: { x: 0, y: 0 },
+              easing: "bounce",
+              duration: 1000,
+              bounces: 4,
+              stiffness: 3,
+            })
+            .scale({
+              from: { x: 1.2, y: 1 },
+              to: { x: 1, y: 1 },
+              easing: "bounce",
+              duration: 1000,
+              delay: 100,
+              bounces: 4,
+              stiffness: 1,
+            })
+            .scale({
+              from: { x: 1, y: 1.2 },
+              to: { x: 1, y: 1 },
+              easing: "bounce",
+              duration: 1000,
+              delay: 100,
+              bounces: 6,
+              stiffness: 1,
+            })
+            .applyTo(n.barDom, {
+              onComplete: function () {
+                promise(function (resolve) {
+                  resolve();
+                });
+              },
+            });
+        },
+        close: function (promise) {
+          var n = this;
+          new Bounce()
+            .translate({
+              from: { x: 0, y: 0 },
+              to: { x: 450, y: 0 },
+              easing: "bounce",
+              duration: 500,
+              bounces: 4,
+              stiffness: 1,
+            })
+            .applyTo(n.barDom, {
+              onComplete: function () {
+                promise(function (resolve) {
+                  resolve();
+                });
+              },
+            });
+        },
+      },
+    }).show();
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -19,7 +111,7 @@ function Product({ title, image, rating, price }) {
         </div>
       </div>
       <img src={image} alt="product" />
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
