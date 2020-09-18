1d76553b20526f778fdbaae3a37434a23a1041b3
Oliver Gomes <ogomesphoto@gmail.com>
2020-09-18 11:32:04 -0400

    Orders initial

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { useStateValue } from "../../StateProvider";
+import { db } from "../../firebase";
+import Order from "../Order/Order.js";
+
+import "./Orders.css";
+
+function Orders() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    db.collection("users")
+      .doc(user?.uid)
+      .collection("orders")
+      .orderBy("created", "desc")
+      .onSnapshot((snapshot) =>
+        setOrders(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
+  return (
+    <div className="orders">
+      <h1>Your Orders</h1>
+
+      <div className="orders__order">
+        {orders?.map((order) => (
+          <Order order={order}></Order>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
